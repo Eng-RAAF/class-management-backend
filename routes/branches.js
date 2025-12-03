fd5192a67be5931a95a57d2e96a51b9c2293e1d9
Eng-RAAF <salaad79@gmail.com>
2025-12-03 17:29:20 +0300

    first commit

--- routes/branches.js
+++ routes/branches.js
@@ -13,6 +13,7 @@ const router = express.Router();
 // Get all branches
 //branchesadda
 //schooladda
+//branchupdate
 router.get('/', async (req, res) => {
   try {
     const branches = await getBranches();
